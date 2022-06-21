a70b3e6d3ecbd29d2efd8a28b345b947974e5444
camilomontoyau <camilo.montoya@techstars.com>
2022-06-21 16:44:21 -0500

    seeding courses

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,30 @@
 // this follows what's recommended here https://planetscale.com/blog/how-to-seed-a-database-with-prisma-and-next-js
 import { PrismaClient } from '@prisma/client'
 import users from './seeds/users'
+import courses from './seeds/courses'
 
 const prisma = new PrismaClient();
 
 
 const load = async () => {
   try {
+    console.log('=== SEEDS START ===')
     // create users
+    console.log('Seeding users....')
     await prisma.user.createMany({
       data: users
     })
+    console.log('Users successfuly seeded')
+
+    // create courses
+    console.log('Seeding courses....')
+    await prisma.course.createMany({
+      data: courses,
+    })
+    console.log('Courses successfuly seeded')
+    
+    
+    console.log('=== SEEDS END ===')
   } catch (e) {
       console.error(e);
       process.exit(1);
---
+++ prisma/seeds/courses.ts
@@ -0,0 +1,24 @@
+import faker from 'faker'
+import { 
+  Course 
+} from '@prisma/client'
+import { v4 as uuidV4 } from 'uuid'
+
+const courses: Array<Course> = [
+  {
+    id: uuidV4(),
+    name: faker.animal.dog(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null
+  },
+  {
+    id: uuidV4(),
+    name: faker.animal.cat(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null
+  },
+]
+
+export default courses
