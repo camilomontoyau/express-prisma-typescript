adce9a1a1e23e8115d369a215b2b41ee05fa2b0a
camilomontoyau <camilo.montoya@techstars.com>
2022-06-24 18:59:49 -0500

    express-openapi-validator

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express'
 import cors from 'cors'
+import * as OpenApiValidator from 'express-openapi-validator';
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: './api.yaml',
+    validateRequests: true, // (default)
+    validateResponses: true, // false by default
+  }),
+)
 
 const PORT = 4000
 
