30f391e807a62073a25b0abd1a0d6e851be39b4f
camilomontoyau <camilo.montoya@techstars.com>
2022-06-24 19:53:19 -0500

    allUsers

--- src/routes/users.controller.ts
+++ src/routes/users.controller.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
 
 const router = Router()
 
-router.get('/', (_, res)=>{
+router.get('/', async(_, res)=>{
+  const allUsers = await prisma.user.findMany()
   res.status(200).json({
-    items: []
+    items: allUsers
   })
 })
 
