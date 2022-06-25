b519f0441a9370a9f34cf8e25ca5a3851e8a3f06
camilomontoyau <camilo.montoya@techstars.com>
2022-06-25 07:22:02 -0500

    users controller

--- src/routes/users.controller.ts
+++ src/routes/users.controller.ts
@@ -1,32 +1,128 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { PrismaClient } from '@prisma/client'
+import { 
+  PrismaClientValidationError,
+} from '@prisma/client/runtime'
 
 const prisma = new PrismaClient();
 
 const router = Router()
 
-router.get('/', async(_, res)=>{
-  const allUsers = await prisma.user.findMany()
-  res.status(200).json({
-    items: allUsers
-  })
+router.get('/', async(_: Request, res: Response)=>{
+  try {
+    // TODO: handle query params here
+    // TODO: handle pagination
+    const allUsers = await prisma.user.findMany()
+    res.status(200).json({
+      items: allUsers,
+      deletedAt: null // TODO: no soft delete in prisma
+    })  
+  } catch (error: PrismaClientValidationError | any) {
+    console.log(error) // TODO: define error logging
+    return res.status(500).send()
+  }
 })
 
-router.get('/:id', (req, res)=>{
-  res.status(200).json({...req.params})
+router.get('/:id', async(req, res)=>{
+  try {
+    const { id }: { id?: string } = req.params
+    const user = await prisma.user.findFirst({
+      where: {
+        id,
+        deletedAt: null, // TODO: no soft delete in prisma
+      },
+    })
+    if(user) return res.status(200).json(user)
+    res.status(404).send()
+  } catch (error: PrismaClientValidationError | any) {
+    console.log(error) // TODO: define error logging
+    return res.status(500).send()
+  }
 })
 
-router.post('/', (req, res)=>{
-  res.status(201).json(req.body)
+router.post('/', async (req, res)=>{
+  try {
+    const { email }: { email?: string } = req.body
+    const user = await prisma.user.upsert({
+      where: {
+        email,
+      },
+      update: {
+        ...req.body,
+        deletedAt: null
+      },
+      create: {
+        ...req.body,
+      },
+    })
+    res.status(201).json(user)  
+  } catch (error: PrismaClientValidationError | any) {
+    console.log(error) // TODO: define error logging
+    return res.status(500).send()
+  }
 })
 
 
-router.put('/:id', (req, res)=>{
-  res.status(200).json(req.body)
+router.put('/:id', async (req, res)=>{
+  try {
+    const { id }: { id?: string } = req.params
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        id,
+        deletedAt: null, // TODO: no soft delete in prisma
+      },
+    })
+    if(existingUser) {
+      const updatedUser = await prisma.user.update({
+        where: {
+          id,
+        },
+        data: {
+          ...req.body,
+        },
+      })
+      return res.status(200).json(updatedUser)
+    } 
+    res.status(404).send()
+  } catch (error: PrismaClientValidationError | any) {
+    console.log(error) // TODO: define error logging
+    return res.status(500).send()
+  }
 })
 
-router.delete('/:id', (req, res)=>{
-  res.status(204).json(req.body)
+router.delete('/:id', async (req, res)=>{
+  try {
+    const { id }: { id?: string } = req.params
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        id,
+        deletedAt: null, // TODO: no soft delete in prisma
+      },
+    })
+    if(existingUser) {
+      /*
+        THIS IS HARD DELETE ONLY
+        const deleteUser = await prisma.user.delete({
+          where: {
+            id,
+          },
+        }) 
+      */
+      await prisma.user.update({
+        where: {
+          id,
+        },
+        data: {
+          deletedAt: null,
+        },
+      })
+      return res.status(204).send()
+    } 
+    res.status(404).send()
+  } catch (error: PrismaClientValidationError | any) {
+    console.log(error) // TODO: define error logging
+    return res.status(500).send()
+  }
 })
 
 export default router
