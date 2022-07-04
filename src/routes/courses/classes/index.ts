e98f6aedbf0807103181a0c706255f36a519e6d8
camilomontoyau <camilo.montoya@techstars.com>
2022-07-04 13:12:26 -0500

    creating routes under /courses/:id/classes

--- src/routes/courses/classes/index.ts
+++ src/routes/courses/classes/index.ts
@@ -7,15 +7,16 @@ import studentsController from './students.controller'
 
 const routes = [
   {
-    path: '/',
-    controller: classesController
-  },
-  {
     path: '/:classId/students',
     controller: studentsController
   },
+  {
+    path: '/',
+    controller: classesController
+  },
 ]
 
+console.log('creating routes under /courses/:id/classes')
 routes.forEach(({ path, controller }) => {
   router.use(path, controller)
   console.log(`route created ${path}`)
