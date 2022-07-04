bc426dbd3059da0368b21a490f4a36f3779f06a9
camilomontoyau <camilo.montoya@techstars.com>
2022-07-04 14:58:20 -0500

    add students to a class

--- src/routes/courses/classes/students.controller.ts
+++ src/routes/courses/classes/students.controller.ts
@@ -7,7 +7,8 @@ import {
   Prisma,
   PrismaClient, 
   Class,
-  Course 
+  Course,
+  User, 
 } from '@prisma/client'
 import { 
   PrismaClientValidationError,
@@ -23,7 +24,8 @@ const select: Prisma.UserSelect = {
   id: true,
   firstName: true,
   lastName: true,
-  email: true,
+  email: true, //
+  role: true,
   createdAt: true,
   updatedAt: true,
 }
@@ -93,6 +95,71 @@ router.get('/', async(req: Request, res: Response)=>{
     }
 
     res.status(404).send()
+  } catch (error: PrismaClientValidationError | any) {
+    console.log(error) // TODO: define error logging
+    return res.status(500).send()
+  }
+})
+
+router.post('/', async (req: Request, res: Response)=>{
+  try {
+    const courseId: string  = req.params.courseId
+    const classId: string  = req.params.classId
+    const userId: string = req.body.userId
+
+    const existingUser: User | null = await prisma.user.findFirst({
+      where: {
+        id: userId,
+        deletedAt: null
+      }
+    })
+
+    if(!existingUser) {
+      return res.status(404).json({
+        id: uuidV4(),
+        message: 'user not found'
+      })
+    }
+
+    const existingCourse = await prisma.course.findFirst({
+      where: {
+        id: courseId,
+        deletedAt: null, // TODO: no soft delete in prisma
+      },
+    })
+
+    if(!existingCourse) {
+      return res.status(404).json({
+        id: uuidV4(),
+        message: 'course not found'
+      })
+    }
+
+    const existingClass: Class | null = await prisma.class.findFirst({
+      where: {
+        id: classId,
+        courseId,
+        deletedAt: null
+      }
+    })
+
+    if(!existingClass) {
+      return res.status(404).json({
+        id: uuidV4(),
+        message: 'class not found'
+      })
+    }
+
+    const newUserClassData = {
+      classId,
+      userId,
+    }
+
+    await prisma.userClasses.create({
+      data: newUserClassData,
+    })
+
+    res.status(201).send()
   } catch (error: PrismaClientValidationError | any) {
     console.log(error) // TODO: define error logging
     return res.status(500).send()
